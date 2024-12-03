9736a320e5b6386d46d35122f0a2f3f62a104728
Anand Kumar Rai <mahakalbhakt7085@gmail.com>
2024-12-03 08:55:13 +0530

    Updated MongoDB connection or other fixes

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(expressSession({
 }));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI,)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('Error connecting to MongoDB:', err));
 
